f5484424dfe1d30723199662427a16818e082432
god <chioarub@gmail.com>
2023-02-09 08:33:06 +0200

    initial

---
+++ src/setupJest.ts
@@ -0,0 +1,14 @@
+import 'jest-preset-angular';
+
+Object.defineProperty(window, 'CSS', {value: null});
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance']
+    };
+  }
+})
